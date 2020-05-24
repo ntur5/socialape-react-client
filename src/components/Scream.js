af3037e454e47bba0c2288e92f44f0ae1c58865d
ntur5 <pressntr@gmail.com>
2020-05-23 18:04:21 -0600

    created the login form

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -8,7 +8,6 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typorgraphy from '@material-ui/core/Typography';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
